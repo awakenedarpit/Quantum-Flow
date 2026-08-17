b4c718fb0cdbf25bea00edc2d9951e1648ede643
awakenedarpit <awakenedarpit@gmail.com>
2026-08-17 20:26:21 +0530

    feat: add Supabase client configuration

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://mtpwnbrzopwwgzbngagr.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_F37TPbCzZoo95gxki_lCXg_uKsZulj4';
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
